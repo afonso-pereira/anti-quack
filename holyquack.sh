c8bd937fec4a3c3eebe50544c2240925df20be7c
Afonso Pereira <54318711+afonso-pereira@users.noreply.github.com>
2020-04-30 07:15:59 +0100

    Anti Bad USB Script
    This script will automatically block every USB port in the hosts you specify whenever a new device is detected.

--- holyquack.sh
+++ holyquack.sh
@@ -8,25 +8,25 @@ lsusb -v > quackhunter.txt; #gets info about connected USB devices
 lsusb -v > quackhunting.txt; #gets info about connected USB devices 
 cmp quackhunter.txt quackhunting.txt > scope.txt; #compares info, if differente, writes "differ" to scope.txt
 if (grep -q 'differ' scope.txt); #checks scope.txt for the word "differ"
-then 
+then #deactivates usb ports (read bellow)
+(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
+(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
+(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
+(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
 (xrandr -q | grep ' connected' | wc -l; > monitorcount1.txt); #gets number of monitors connected 
 cmp monitorcount0.txt monitorcount1.txt > scope.txt; #compares info, if differente, writes "differ" to scope.txt
-fi;
 if (grep -q 'differ' scope.txt); 
-#run 'lsusb -t' 
-#plug you USB device on each port and replace <bus> with the usb host Bus number
-#duplicate the lines under according to your needs 
-#if you need more help, check readme file :p
 then    #activates usb ports
 (echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/bind);
 (echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/bind);
 (echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/bind);
 (echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/bind);
-else    #deactivates usb ports 
-(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
-(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
-(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
-(echo '<bus>-1'| sudo tee /sys/bus/usb/drivers/usb/unbind);
 
 fi;
+fi;
 done;
+#plug you USB device on each port and use the command 'lsusb -t' to get host bus number
+#replace <bus> with the usb host Bus number
+#duplicate the lines under according to your needs 
+#to reactivate the ports, you need to plug in a dummy monitor (a regular vga cable worked for me, but please test it first x)
+#if you need more help, contact me :p
